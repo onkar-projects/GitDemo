4e454a5d17ebeed9f1df3fc30136b30af576f218
onkar-projects <kure.onkar41@gmail.com>
2024-03-23 17:12:45 +0530

    demo git

---
+++ DemoProject/src/Demo_pkg/Addition.java
@@ -0,0 +1,15 @@
+package Demo_pkg;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		
+		int a = 19;
+		int b = 5;
+		int sum ;
+		
+		sum = a +b;
+		
+		System.out.println("sum = "+ sum);
+	}
+}
